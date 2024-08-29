91f7144e178385fdb23e047a9fe6efd59af7be38
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-08-28 23:23:49 -0700

    Work in progress:Adding toString method to DVDCollection

--- src/DVDCollection.java
+++ src/DVDCollection.java
@@ -6,5 +6,14 @@ public class DVDCollection {
 	private String sourceName;
 	private boolean modified;
 	
+	public String toString() {
+		String header;
+		header = String.format("numdvds = " + Integer.toString(this.numdvds) +
+				"dvdArray.length = " + Integer.toString(this.numdvds));
 	
+		for (int i = 0; i < numdvds; i ++) {
+			
+		}
+		
+	}
 }
