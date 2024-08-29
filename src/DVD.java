3274e30da4f9aeaab81ef967661f9308e91a1655
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-08-28 22:20:17 -0700

    completed DVD constructor

--- src/DVD.java
+++ src/DVD.java
@@ -1,4 +1,14 @@
 
 public class DVD {
-
+	private String title;
+	private String rating;
+	private int runningTime;
+	
+	public DVD(String dvdTitle, String dvdRating, int dvdRunningTime) {
+		this.title = dvdTitle;
+		this.rating = dvdRating;
+		this.runningTime = dvdRunningTime;
+	}
+	
+	
 }
